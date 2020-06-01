133d423dbf9538b63456e7bb6eebb8e5fcad0bd0
lyunfun <958540785@qq.com>
2020-06-01 21:00:05 +0800

    add files

---
+++ lyunfun-loadash.js
@@ -0,0 +1,16 @@
+var lyunfun = {
+  isNull: function(val) {
+    if (val === null) {
+      return true
+    } else {
+      return false
+    }
+  },
+  isNaN: function(val) {
+    if (val !== val) {
+      return true
+    } else {
+      return false
+    }
+  },
+}
